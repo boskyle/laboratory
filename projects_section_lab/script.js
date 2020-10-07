14b64dc67a706bd8f99ed6b38e75f91e0a08f986
Boswell Orendain <boslearning22@gmail.com>
2020-10-07 17:13:10 -0400

    added button category change on project

--- projects_section_lab/script.js
+++ projects_section_lab/script.js
@@ -1,9 +1,57 @@
 $(document).ready(function() {
    
     
-    $('#button').click(function(){
-        
-       $('#removed').remove();
+    $('#btn-all').click(function(){
+        $('.flex-item').show();
+        $('.btn').removeClass('active');
+        $('#btn-all').addClass('active');
+      
     });
+
+    // delete all with class that dont have react
+    $('#btn-react').click(function(){
+        
+        $('.flex-item').hide();
+        $('.btn').removeClass('active');
+
+        if ($('.flex-item').hasClass('react')){
+           $('.react').show();
+           $('#btn-react').addClass('active');
+        }
+     });
+
+     $('#btn-nodejs').click(function(){
+        $('.flex-item').hide();
+        $('.btn').removeClass('active');
+        if ($('.flex-item').hasClass('nodejs')){
+           $('.nodejs').show();
+           $('#btn-nodejs').addClass('active');
+        }
+     
+     });
+
+     $('#btn-css').click(function(){
+        $('.flex-item').hide();
+        $('.btn').removeClass('active');
+        if ($('.flex-item').hasClass('css')){
+           $('.css').show();
+           $('#btn-css').addClass('active');
+        }
+     });
+
+     $('#btn-python').click(function(){
+        $('.flex-item').hide();
+        $('.btn').removeClass('active');
+
+        if ($('.flex-item').hasClass('python')){
+           $('.python').show();
+            // add a bootstrap class when clicked 'active'
+           $('#btn-python').addClass('active');
+        }
+     });
+ 
+     
+
+
 
 });
