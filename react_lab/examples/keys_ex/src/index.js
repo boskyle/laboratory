7a914b394601a92c0013c27f069f9fe8d3f755ff
Boswell Orendain <boslearning22@gmail.com>
2020-11-07 14:02:29 -0500

    new files i actually forgot what i added

---
+++ react_lab/examples/keys_ex/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+// const title = React.createElement(
+//   'h1', {id: 'title', className: 'header'},'Hello World!'
+// )
+
+
+var booklist = [
+  {'title':'Hunger','author':'Roxane Gay','pages':320},
+  {'title':'Boat','author':'I am not Gay','pages':104}
+]
+
+// arrow function, instead of the traditional function
+const Book = ({title,author,pages}) => {
+
+return (
+  <section> 
+    <h2>{title}</h2>
+    <p>by: {author}</p>
+    <p>Pages: {pages} pages.</p>
+  </section>
+);
+}
+
+
+const Library = ({books}) => {
+  return (
+    <div>
+      {/* maps over an object with the key-pair */}
+      {books.map(
+        (book, i) =>
+        <Book
+        key={i}
+        title={book.title}
+        author={book.author}
+        pages={book.pages}
+        />
+      )}
+
+    </div>
+  )
+
+}
+
+
+
+
+
+ReactDOM.render(
+  
+  <Library books={booklist}/>,
+  document.getElementById('root')
+);
+
