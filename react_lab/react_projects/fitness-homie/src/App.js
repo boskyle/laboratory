e4dec22b9bd2601b2a5f970cd9b6df0a43f34b0d
Boswell Orendain <boslearning22@gmail.com>
2020-12-09 18:43:16 -0500

    added fitness homie

---
+++ react_lab/react_projects/fitness-homie/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// comment from main branch
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello!!</h1>
+    </div>
+  );
+}
+
+export default App;
