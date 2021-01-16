ba4d078b5136069b920086e44ee56a3c6fa9f39b
Boswell Orendain <boskyle.orendain@gmail.com>
2021-01-16 15:30:18 -0500

    made progress with dashboard css layout

--- react_lab/react_projects/fitness-homie/src/Dashboard/Dashboard.js
+++ react_lab/react_projects/fitness-homie/src/Dashboard/Dashboard.js
@@ -4,7 +4,9 @@ import { Redirect} from "react-router-dom";
 import {useHistory} from 'react-router-dom';
 import Navigation from '../Navigation';
 import {loadFromLocalStorage} from '../LocalStorage';
-import {LoadBasicInfo} from './db-endpoints/loadProfile';
+import {LoadBasicInfo,LoadFitnessInfo} from './db-endpoints/loadProfile';
+import '../assets/fonts/index.css';
+import './dashboard.css';
 
 
 
@@ -26,9 +28,24 @@ function Dashboard() {
         country: '',
     });
 
-   
+    const [userFitness,setUserFitness] = useState({
+        age: '',
+        heightCm: '',
+        weightLbs: '',
+        gender: '',
+        activity: '',
+        bmr:'',
+        calories: ''
+    });
+
+ 
+
+    
     useEffect( () => {
         // this async function returns false when userInfo hasnt ben filled yet
+
+        let isCancelled = false;
+
         LoadBasicInfo(userInfo.user_id)
         .then(data => {
             // only
@@ -37,8 +54,8 @@ function Dashboard() {
                     pathname: '/login/setup',
                     isDataGiven: data
                 })
-            } else {
-                console.log(data);            
+            } else {     
+                if (!isCancelled)     
                 setUserInfo({
                     user_id:loadFromLocalStorage("isLogged").isLogged[1],
                     username: data.username,
@@ -50,24 +67,62 @@ function Dashboard() {
             }
        
         })
-        // redirect to /login/setup
-
-        
-    },[history,userInfo.user_id])
 
 
+        LoadFitnessInfo(userInfo.user_id)
+        .then(data => {
+            // only
+            if (data === false && loadFromLocalStorage('isLogged').isLogged[0] === true) {
+                history.push({
+                    pathname: '/login/setup',
+                    isDataGiven: data
+                })
+            } else {     
+                if (!isCancelled)     
+                setUserFitness({
+                    age: data.age,
+                    heightCm: data.height_cm,
+                    weightLbs: data.weight_lbs,
+                    gender: data.gender,
+                    activity: data.activity_level,
+                    bmr: data.BMR,
+                    calories: data.calories
+                })
+            }
+       
+        })
 
-    console.log(userInfo);
+        // prevents memory leak, make sure that it is mounted first
+        return () => {
+            isCancelled = true;
+        }    
+    },[])
+
+
+
  
 
    
    
     if (loadFromLocalStorage("isLogged").isLogged[0] === true) {
             
-        return <div className="container-fluid text-center">
-        <Navigation/>
-        <h2>{userInfo.firstname}</h2>
-    </div> 
+        return  <div className="container-fluid">
+                    <Navigation/>
+                        <div className="container-flex">
+                            <div className="flex-item" id="flex-item-profile">
+                                <h2>PROFILE</h2>
+                            </div>
+
+                            <div className="flex-item" id="flex-item-journal">
+                                <h2>JOURNAL</h2>
+                            </div>
+
+                            <div className="flex-item" id="flex-item-workout">
+                                <h2>WORKOUT</h2>
+                            </div>
+
+                        </div>
+                </div> 
     } else
    return <Redirect to="/" />;
   
