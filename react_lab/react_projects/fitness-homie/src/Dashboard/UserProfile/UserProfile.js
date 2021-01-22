66e4ba1363f4259c7817e7378babcea11f7b5ae5
boskyle <boskyle.orendain@gmail.com>
2021-01-22 17:46:24 -0500

    made dashboard css adjustment to make it more dynamic

--- react_lab/react_projects/fitness-homie/src/Dashboard/UserProfile/UserProfile.js
+++ react_lab/react_projects/fitness-homie/src/Dashboard/UserProfile/UserProfile.js
@@ -11,14 +11,25 @@ const UserProfile = ({username,firstname,lastname,country,usernameSearched}) => 
                 <div className="user-profile-container">
                     <div className="user-profile-item">
                     <h2>PROFILE</h2>
-                    <h3>{firstname +" "+lastname+" "}({username})</h3>
-                    
+                        <div>
+                        <span className="userName">{username}</span>
+                        <span className="name">{firstname +" "+lastname}</span>
+                        </div>
                     </div>
 
 
-                    <div className="user-profile-item">Journal</div>
-                    <div className="user-profile-item">Workout</div>
-              
+                    <div className="user-profile-item">
+                        <h3 className="m-1">JOURNAL</h3>
+                        <div className="journal-items-container">
+                            <div className="journal-item"><h4>STATS</h4></div>
+                            <div className="journal-item"><h4>PROGRESS</h4></div>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className="user-profile-item"><h3>WORKOUT</h3></div>      
                 </div>  
             );
         } else {
