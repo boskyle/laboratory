6548f7690ea93cec4076d713274d78bc36f5c80c
boskyle <boskyle.orendain@gmail.com>
2021-01-24 16:35:57 -0500

    added responsiveness of edit button

--- react_lab/react_projects/fitness-homie/src/Dashboard/UserProfile/Popup.js
+++ react_lab/react_projects/fitness-homie/src/Dashboard/UserProfile/Popup.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import {useState,useEffect} from 'react';
+import Modal from 'react-modal';
 import './popup.css';
 
-
 export default () => {
+
+
+
+    Modal.setAppElement('.App');
+    const [showPop,setShowPop] = useState(false);
+
+    const  handleOpen = (e) => {
+        console.log("open");
+        setShowPop(true);
+    }
+
+    const  handleClose = (e) => {
+        console.log("close");
+        setShowPop(false);
+    }
+
     return (
-    <h2></h2>
+        <>
+        <button  type="button" className="btn edit-button m-0 mb-1" onClick={handleOpen}>Edit</button>
+        <Modal 
+           isOpen={showPop}
+           onRequestClose={handleClose}
+           contentLabel="Minimal Modal Example"
+           className="popUp"
+           overlayClassName="overlay"
+        >
+           
+            <button className="btn save-button" onClick={handleClose}><h2>Save</h2></button>
+           
+          
+        </Modal>
+        </>
     );
    
 };
